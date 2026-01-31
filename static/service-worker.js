2aed23ef7214477b5f5b4956c4de77ec1d4fbc50
wilquemessias <wilquemessias@gmail.com>
2026-01-30 23:01:29 -0300

    Add theme color, icons, and manifest for PWA support; enhance responsiveness with media queries

---
+++ static/service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'besavemd-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  '/static/icons/icon-192x192.png',
+  '/static/icons/icon-512x512.png',
+  // Adicione outros arquivos estáticos que você quer cachear
+  // Por exemplo: css, js (se separados), etc.
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
